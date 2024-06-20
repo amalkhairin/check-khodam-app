2d1e95e839e6f88704869f5269d47360dbb90d43
amalkhairinn <amalkhairinn@gmail.com>
2024-06-20 11:51:05 +0700

    change language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="id">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
